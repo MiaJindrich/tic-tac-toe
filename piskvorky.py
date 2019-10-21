27fbb06e033432e5b3d8f3489ec1e2ddf9d20f95
Mia Jindrichova <jindrich.lucie@gmail.com>
2019-10-22 00:07:43 +0200

    počítač už i umí blokovat

--- piskvorky.py
+++ piskvorky.py
@@ -1,9 +1,9 @@
 from random import randrange
 
 def vyhodnot(pole):
-    if "xxx" in pole:
+    if "x" * 3 in pole:
         vysledek = "Vyhrály křížky!"
-    elif "ooo" in pole:
+    elif "o" * 3 in pole:
         vysledek = "Vyhrály kolečka!"
     elif "-" not in pole:
         vysledek = "Remíza!"
@@ -24,18 +24,25 @@ def tah_hrace(pole, symbol):
     print (pole)
     return pole
  
-def tah_pocitace(pole, pocitac_symbol):
+def tah_pocitace(pole, pocitac_symbol, symbol):
     print ("Hraje počítač.")
     vole_misto = "-"
     tri_prasatka = pocitac_symbol + pocitac_symbol + vole_misto
     tri_prasatka_nadruhou = vole_misto + pocitac_symbol + pocitac_symbol
+    tri_selatka = symbol + symbol + vole_misto
+    tri_selatka_natruhou = vole_misto + symbol + symbol
     # zkontroluju, jestli počítač má dva symboly vedle sebe
     if tri_prasatka in pole:
         pole = pole.replace(tri_prasatka, pocitac_symbol * 3)
     elif tri_prasatka_nadruhou in pole:
         pole = pole.replace(tri_prasatka_nadruhou, pocitac_symbol * 3)
     # pak zkontroluju, jestli hráč nemá dva symboly vedle sebe
-
+    elif tri_selatka in pole:
+        cast_pole = tri_selatka.replace(vole_misto, pocitac_symbol)
+        pole = pole.replace(tri_selatka, cast_pole)
+    elif tri_selatka_natruhou in pole:
+        cast_pole_nadruhou = tri_selatka_natruhou.replace(vole_misto, pocitac_symbol)
+        pole = pole.replace(tri_selatka_natruhou, cast_pole_nadruhou)
     else:
         cislo_policka = randrange (20)
         while "-" not in pole[cislo_policka - 1]:
@@ -56,7 +63,7 @@ def piskvorky1d():
     while vysledek == "-":
         pole = tah_hrace(pole, symbol)
         vysledek = vyhodnot (pole)
-        pole = tah_pocitace (pole, pocitac_symbol)
+        pole = tah_pocitace (pole, pocitac_symbol, symbol)
         vysledek = vyhodnot (pole)
     print (vyhodnot(pole))
 
